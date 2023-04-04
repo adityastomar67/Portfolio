a7c5c42e66009b8d958fabf9aebd94bf67959c9f
Aditya Singh Tomar <adityastomar12@gmail.com>
2023-04-04 08:13:48 +0530

    additions

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <header className="w-full px-32 py-8 font-medium">
+            <nav>
+                <Link href='/'>Home</Link>
+                <Link href='/'>Home</Link>
+                <Link href='/'>Home</Link>
+                <Link href='/'>Home</Link>
+            </nav>
+        </header>
+    );
+};
+
+export default NavBar;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import { Montserrat } from 'next/font/google';
 import Head from 'next/head';
+import NavBar from '@/components/NavBar';
 
 const montserrat = Montserrat({
     subsets: ['latin'],
@@ -18,6 +19,7 @@ export default function App({ Component, pageProps }) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+                <NavBar />
                 <Component {...pageProps} />
             </main>
         </>
