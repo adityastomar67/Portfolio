42a0b85ed7fe12dda00bace0d59c0a20902112f9
Aishwarya Shrivastava <adityastomar12@gmail.com>
2023-04-22 09:36:13 +0530

    About page update

---
+++ src/components/Experience.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div className='my-64'>
+      <h2 className='font-bold'>Experience</h2>
+    </div>
+  )
+}
+
+export default Experience
